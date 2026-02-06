1b9676520e214477f462e17fbbd3b6ca1567ee98
Nabati Hidayatov <nabati1995@gmail.com>
2026-02-06 13:47:23 +0400

    add SumOfOdd and Time classes for calculating sum of odd numbers and converting seconds to hours, minutes, and seconds

---
+++ Time.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Time {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int hours = a / 3600;
+        int b = a % 3600;
+        int minutes = b / 60;
+        int seconds = b % 60;
+        System.out.println(hours + " " + minutes + " " + seconds);
+    }
+}
